2f53739a866adf27e1c09c572b5c113678590869
franmartos11 <franmartos11@gmail.com>
2024-05-08 14:56:01 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex">
+      
+      <img className="flex content-center align-middle" alt="hero" src={"/hero.jpg"}></img>
+    </div>
+  );
+}
